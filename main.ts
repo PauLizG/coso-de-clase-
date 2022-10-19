08c3f1195cd4797ac27710339912b627062311fa
PauLizG <112582743+PauLizG@users.noreply.github.com>
2022-10-19 13:50:26 -0700

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,66 @@
 input.onPinPressed(TouchPin.P0, function () {
     control.reset()
 })
-let H = game.createSprite(0, 2)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    H.change(LedSpriteProperty.Y, -1)
+    basic.pause(200)
+})
+input.onButtonPressed(Button.A, function () {
+    H.change(LedSpriteProperty.X, -1)
+    basic.pause(200)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    H.change(LedSpriteProperty.Y, 1)
+    basic.pause(200)
+})
+input.onButtonPressed(Button.B, function () {
+    H.change(LedSpriteProperty.X, 1)
+    basic.pause(200)
+})
+let H: game.LedSprite = null
+H = game.createSprite(0, 2)
 let V = game.createSprite(randint(0, 4), randint(0, 4))
 let V2 = game.createSprite(randint(0, 4), randint(0, 4))
 let V3 = game.createSprite(randint(0, 4), randint(0, 4))
 H.set(LedSpriteProperty.Blink, 450)
+let Puntuacion = 0
 basic.forever(function () {
-    while (input.buttonIsPressed(Button.A)) {
-        H.change(LedSpriteProperty.X, -1)
-        basic.pause(200)
-    }
-    while (input.buttonIsPressed(Button.B)) {
-        H.change(LedSpriteProperty.X, 1)
-        basic.pause(200)
-    }
-    while (input.logoIsPressed()) {
-        H.change(LedSpriteProperty.Y, -1)
-        basic.pause(200)
-    }
-    while (input.pinIsPressed(TouchPin.P2)) {
-        H.change(LedSpriteProperty.Y, 1)
-        basic.pause(200)
-    }
     if (H.isTouching(V)) {
         V.delete()
+        Puntuacion += 1
+        basic.showIcon(IconNames.Yes)
     }
     if (H.isTouching(V2)) {
         V2.delete()
+        Puntuacion += 1
+        basic.showIcon(IconNames.Yes)
     }
     if (H.isTouching(V3)) {
         V3.delete()
+        Puntuacion += 1
+        basic.showIcon(IconNames.Yes)
     }
-    if (V.isDeleted() && (V2.isDeleted() && V3.isDeleted())) {
+    if (Puntuacion == 3) {
         music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.OnceInBackground)
         H.delete()
         V.delete()
-        for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 2; index++) {
             basic.showIcon(IconNames.Yes)
         }
-        basic.showString("GAME OVER")
+        basic.showString("FELICIDADES")
+    }
+})
+loops.everyInterval(3000, function () {
+    if (!(V.isDeleted())) {
+        V.delete()
+        V = game.createSprite(randint(0, 4), randint(0, 4))
+    }
+    if (!(V2.isDeleted())) {
+        V2.delete()
+        V2 = game.createSprite(randint(0, 4), randint(0, 4))
+    }
+    if (!(V3.isDeleted())) {
+        V3.delete()
+        V3 = game.createSprite(randint(0, 4), randint(0, 4))
     }
 })
